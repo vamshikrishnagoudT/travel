da47016300e574ae803145d8ef4cd453d2fa7a7e
vamshi <vamshigoud00001@gmail.com>
2025-01-01 22:58:43 +0530

    assignment

--- travel website/main/main/settings.py
+++ travel website/main/main/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'main.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'telusko',
+        'NAME':'admin',
         'USER':'postgres',
         'PASSWORD':'admin',
         'HOST':'localhost'
